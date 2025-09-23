9ef280fee1995589448634e7486d7d9476e4bfdf
mestrevh <victor-torugo@hotmail.com>
2025-09-23 17:36:32 -0300

    feat: added the terminal with err in the transmit usart

--- projeto1/projeto1/main.asm
+++ projeto1/projeto1/main.asm
@@ -5,13 +5,17 @@
 ; Author : VictorHugo
 ;
 
+#define CLOCK 16.0e6 ; 16.0e6 s
+#define DELAY 1 ; 1 micro-segundos
 
 .def temp = r16
 .def count_u = r17
 .def count_d = r18
 .def state = r19
+.def timer = r20
 .equ display1 = 0b00010000 ;quinto bit settado
 .equ display2 = 0b00100000 ;sexto bit settado
+
 .cseg
 
 .org 0x0000
@@ -20,10 +24,12 @@ jmp reset
 jmp OCI1A_Interrupt
 
 reset:
-	#define CLOCK 16.0e6
+
+	;CONFIGURAÇÃO DO TIMER
+	
 	.equ PRESCALE = 0b101 ; verifique no datasheet ou slide
 	.equ PRESCALE_DIV = 1024; verifique no datasheet ou slide
-	#define DELAY 0.01 ; 1 micro-segundos
+	
 	.equ WGM = 0b0100 ; configuração do ctc
 	.equ TOP = int(0.5 + ((CLOCK/PRESCALE_DIV)*DELAY)) ; de 0 até 65535
 	.if TOP > 65535
@@ -59,7 +65,28 @@ reset:
 	ldi temp, (1 << PCINT5) | (1 << PCINT4) | (1 << PCINT3) | (1 << PCINT2) | (1 << PCINT1) | (1 << PCINT0)
 	out DDRB, temp
 
-	ldi state, 0b00001010
+	; CONFIGURAÇÃO DA USART
+	; Configura baud rate
+	
+	.equ baud = 9600
+	.equ ubrr_value = int((CLOCK / (16 * baud)) - 1)
+
+	ldi temp, high(ubrr_value)
+	sts UBRR0H, temp
+	ldi temp, low(ubrr_value)
+	sts UBRR0L, temp
+
+	; Habilita TX
+	ldi temp, (1<<TXEN0)
+	sts UCSR0B, temp
+
+	; Modo assíncrono, 8 bits, 1 stop, sem paridade
+	ldi temp, (1<<UCSZ01) | (1<<UCSZ00)
+	sts UCSR0C, temp
+
+	;iniciar no estado e2
+	ldi timer, 10
+	ldi state, 2
 
 	sei
 	
@@ -69,38 +96,40 @@ main:
 	mov temp, count_d
 	ori temp, display1; 0b00100000 - display 1
 	out PORTC, temp
+	rcall delay_small
 
 	;enviando os bits do led para unidades
 	mov temp, count_u
 	ori temp, display2; 0b00010000 - display 2
 	out PORTC, temp
+	rcall delay_small
+
+	;timer = registrador responsável pelo tempo em relação a troca dos estados
+	;mov timer, count_d
 	
-	;state = registrador responsável pela troca dos estados
-	;mov state, count_d
-	
-	cpi state, 0b00000000 ; 0
+	cpi timer, 0b00000000 ; 0
 	breq state_one
 
-	cpi state, 0b00001010 ; 10
+	cpi timer, 0b00001010 ; 10
 	breq state_two
 
-	cpi state, 0b00100000 ; 32
+	cpi timer, 0b00100000 ; 32
 	breq state_three
 
-	cpi state, 0b00100100 ; 36
+	cpi timer, 0b00100100 ; 36
 	breq state_four
 
-	cpi state, 0b01100001 ; 97
+	cpi timer, 0b01100001 ; 97
 	breq state_five
 
-	cpi state, 0b01100101 ; 101
+	cpi timer, 0b01100101 ; 101
 	breq state_six
 
-	cpi state, 0b01111111 ; 127
+	cpi timer, 0b01111111 ; 127
 	breq state_seven
 
-	cpi state, 0b10000011 ; 131
-	breq zero
+	cpi timer, 0b10000011 ; 131
+	breq clear
 
 	rjmp main
 		
@@ -110,6 +139,13 @@ main:
 
 		ldi temp, 0b00100100
 		out PORTB, temp
+
+		inc state
+
+		; transmite o estado pela UART
+		ldi r24, 'H'
+		rcall uart_transmit
+
 		rjmp main
 
 	state_two:
@@ -121,6 +157,13 @@ main:
 
 		ldi count_u, 0b00000111 ; timer unidade = 7
 		ldi count_d, 0b00001000 ;  timer dezena = 8
+
+		inc state
+		
+		; transmite o estado pela UART
+		mov r24, state
+		rcall uart_transmit
+
 		rjmp main
 
 	state_three:
@@ -129,6 +172,12 @@ main:
 
 		ldi temp, 0b00100010
 		out PORTB, temp
+
+		inc state
+		; transmite o estado pela UART
+		ldi r24, 'L'
+		rcall uart_transmit
+
 		rjmp main
 
 	state_four:
@@ -137,6 +186,9 @@ main:
 
 		ldi temp, 0b00001100
 		out PORTB, temp
+
+		inc state
+
 		rjmp main
 
 	state_five:
@@ -148,6 +200,8 @@ main:
 		
 		ldi count_u, 0b00000100 ; =4
 		ldi count_d, 0b00000000
+
+		inc state
 		
 		rjmp main
 
@@ -161,6 +215,8 @@ main:
 		ldi count_u, 0b00000000
 		ldi count_d, 0b00000100 ; = 4
 
+		inc state
+
 		rjmp main
 
 	state_seven:
@@ -169,11 +225,14 @@ main:
 
 		ldi temp, 0b00100100
 		out PORTB, temp
+
+		inc state
+
 		rjmp main
 
-	zero:
-		ldi count_u, 0x00
-		ldi count_d, 0x00
+	clear:
+		ldi timer, 0x00
+		ldi state, 0x00
 
 	rjmp main
 
@@ -184,7 +243,7 @@ OCI1A_Interrupt:
 	push temp
 
 	;conta cada segundo para o estado
-	inc state
+	inc timer
 
 	;contar unidade
 	dec count_u
@@ -200,11 +259,11 @@ OCI1A_Interrupt:
 	dec count_d
 
 	;comparar quando a unidade chegar em '10'
-	;cpi count_d, 10
-	;brne skip
+	cpi count_d, 0xff
+	brne skip
 
 	;zerar dezenas
-	;ldi count_d, 0x00
+	ldi count_d, 0x00
 
 	skip:
 		
@@ -227,3 +286,24 @@ OCI1A_Interrupt:
 	out SREG, temp
 	pop temp
 	reti
+
+.def byte_tx = r24
+uart_transmit:
+    lds temp, UCSR0A
+    sbrs temp, UDRE0        ; espera até buffer ficar livre
+    rjmp uart_transmit
+    sts UDR0, byte_tx       ; transmite caractere
+    ret
+.undef byte_tx
+
+; 20 * 250 = 5000 ciclos
+delay_small:
+    ldi r24, 20        ; contador externo
+outer_loop:
+    ldi r25, 250       ; contador interno
+inner_loop:
+    dec r25            ; decrementa o contador interno
+    brne inner_loop    ; repete até zerar
+    dec r24            ; quando termina o interno, decrementa o externo
+    brne outer_loop
+    ret
